f1ee5bad5857a1d4cae4b2f4d960a8eba5e0838d
WiJE96 <lahiruwijerathna1996@gmail.com>
2022-11-18 22:35:01 -0500

    detector updated

--- detector.m
+++ detector.m
@@ -1,16 +1,21 @@
 function [freq_sign, freq_offset] = detector(burst_sig)
-% burst_sig (complex)     - burst signal in the begining of the frame
-
+% INPUT : burst_sig (complex)   - burst signal in the begining of the frame 
+% OUTPUTS:
+%       freq_sign               - group freq signature
+%       freq_offset             - (group freq signature -
+%       128*(burst_sig_freq/(2*pi)) (i.e. Delta_omega)
+    
 group_bins = [10 20 30 40 50 60 70 80 90 100 110];
 
 % Get the fft
 dft_out = abs(fft(burst_sig));
 
 % Detect burst freq
-[max_fft, max_bin] = max(dft_out);
+[max_fft, max_bin] = max(dft_out(1:64));
 
 % Calculate freq signature and freq offset
-dist_arr = group_bins - max_bin;
+max_bin = max_bin -1;
+dist_arr = abs(group_bins - max_bin);
 [min_dist, min_dist_idx] = min(dist_arr);
 
 freq_sign = group_bins(min_dist_idx);
