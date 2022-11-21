3adc10e3ccc362004560dac1bc9647bab28de18e
Pasan Dissanayake <pasandissanayake@gmail.com>
2022-11-20 21:03:31 -0500

    asaasasaasa

--- Top_level_read_file.m
+++ Top_level_read_file.m
@@ -3,7 +3,9 @@ function decoded_data = Top_level_read_file(path, F0, F1)
 FREQ_ID=10; GROUP_ID=0; FRAME_START=0;
 
 block_num = 1; frame = FRAME_START;
-decoded_data = [];
+ddd = [];
+n_end = 0;
+figure;
 while true
     [id_sig,data_sig]=read_file(path,block_num,0);
 
@@ -15,13 +17,22 @@ while true
     
     if freq_sign==FREQ_ID
         owner = 'us';
-        decoded_data = [decoded_data polyphase_rx(scrambler(interleaver_rx(data_sig),GROUP_ID,frame),F0,F1)];
+        n = 0:1:(length(data_sig)-1);
+        n = n' + n_end;
+        n_end = n(end) + 1;
+        fix = exp(-2*pi*1i*50/128*n);
+        plot(n,real(fix),'r-',n,imag(fix),'b-');
+        hold on
+        d_data = data_sig .* fix;
+        ddd = [ddd polyphase_rx(scrambler(interleaver_rx(d_data),GROUP_ID,frame),F0,F1);];
+        frame = frame + 1;
     else
         owner = 'them';
     end
     fprintf('block: %2d freq. sign: %2d offset: %2d owner: %4s\n',block_num,freq_sign,freq_offset,owner);
     
     block_num = block_num + 1;
-    frame = frame + 1;
+end
+decoded_data = ddd;
 end
 
--- test.m
+++ test.m
@@ -6,14 +6,17 @@ H1 = H0; H1(1,16) = -1024;
 F0 = H0;
 F1 = H1; %F1(1,:) = -F1(1,:);
 
-dd = Top_level_read_file('C:\Users\pasan\Desktop\ENEE630_F2022_TXsim\Rn_GroupID_0_FreqID_10_SNR_100.000000_Foff_0.000000_Nframe_5_Nstart_0.txt',F0,F1);
+p = '_GroupID_0_FreqID_10_SNR_100.000000_Foff_0.000000_Nframe_5_Nstart_0.txt';
+
+dd = Top_level_read_file(strcat('C:\Users\pasan\Desktop\ENEE630_F2022_TXsim\Rn', p),F0,F1);
 
 de = [];
 for k=1:5
-    [a,e] = read_file('C:\Users\pasan\Desktop\ENEE630_F2022_TXsim\Input_GroupID_0_FreqID_10_SNR_100.000000_Foff_0.000000_Nframe_5_Nstart_0.txt',k,1);
+    [a,e] = read_file(strcat('C:\Users\pasan\Desktop\ENEE630_F2022_TXsim\Input', p),k,1);
     de = [de e'];
 end
 
+figure;
 plot(1:length(dd),abs(dd),'r-',1:length(de),abs(de),'b--')
 
 figure;
@@ -21,7 +24,5 @@ subplot(2,1,1);
 plot(abs(fft(dd,2^13))/5120,'r-');
 subplot(2,1,2);
 plot(abs(fft(de,2^13))/5120,'r-');
-
-
 
 fprintf('test MSE: %f\n',MSE(dd,de,0,0,0))
