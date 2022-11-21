8587843d4234df3aceac3f907f857d4c6b6ae188
Pasan Dissanayake <pasandissanayake@gmail.com>
2022-11-21 13:05:42 -0500

    scrambled scrambler

--- Top_level_read_file.m
+++ Top_level_read_file.m
@@ -1,11 +1,18 @@
-function decoded_data = Top_level_read_file(path, F0, F1)
+function decoded_data = Top_level_read_file(path)
 
-FREQ_ID=10; GROUP_ID=0; FRAME_START=0;
+FREQ_ID=10; GROUP_ID=0; FRAME_START=0; DFT_BINS = 128;
+
+global H0 H1 F0 F1
+H0 = [-1 0 3 0 -8 0 21 0 -45 0 91 0 -191 0 643 1024 643 0 -191 0 91 0 -45 0 21 0 -8 0 3 0 -1];
+H0 = [H0; [2048 zeros(1,30)]];
+H1 = H0; H1(1,16) = -1024;
+
+F0 = H0;
+F1 = H1; %F1(1,:) = -F1(1,:);
 
 block_num = 1; frame = FRAME_START;
-ddd = [];
+decoded_data = [];
 n_end = 0;
-figure;
 while true
     [id_sig,data_sig]=read_file(path,block_num,0);
 
@@ -20,11 +27,9 @@ while true
         n = 0:1:(length(data_sig)-1);
         n = n' + n_end;
         n_end = n(end) + 1;
-        fix = exp(-2*pi*1i*50/128*n);
-        plot(n,real(fix),'r-',n,imag(fix),'b-');
-        hold on
-        d_data = data_sig .* fix;
-        ddd = [ddd polyphase_rx(scrambler(interleaver_rx(d_data),GROUP_ID,frame),F0,F1);];
+        freq_adjust = exp(-1i*2*pi*freq_offset*n/DFT_BINS);
+        d_data = data_sig .* freq_adjust;
+        decoded_data = [decoded_data polyphase_rx(scrambler(interleaver_rx(d_data),GROUP_ID,frame),F0,F1);];
         frame = frame + 1;
     else
         owner = 'them';
@@ -33,6 +38,5 @@ while true
     
     block_num = block_num + 1;
 end
-decoded_data = ddd;
 end
 
--- test.m
+++ test.m
@@ -1,18 +1,10 @@
-global H0 H1 F0 F1
-H0 = [-1 0 3 0 -8 0 21 0 -45 0 91 0 -191 0 643 1024 643 0 -191 0 91 0 -45 0 21 0 -8 0 3 0 -1];
-H0 = [H0; [2048 zeros(1,30)]];
-H1 = H0; H1(1,16) = -1024;
+p = '_GroupID_0_FreqID_10_SNR_100.000000_Foff_234.375000_Nframe_5_Nstart_0.txt';
 
-F0 = H0;
-F1 = H1; %F1(1,:) = -F1(1,:);
-
-p = '_GroupID_0_FreqID_10_SNR_100.000000_Foff_0.000000_Nframe_5_Nstart_0.txt';
-
-dd = Top_level_read_file(strcat('C:\Users\pasan\Desktop\ENEE630_F2022_TXsim\Rn', p),F0,F1);
+dd = Top_level_read_file(strcat('C:\Users\pasan\Desktop\ENEE630_F2022_TXsim\Rn', p));
 
 de = [];
 for k=1:5
-    [a,e] = read_file(strcat('C:\Users\pasan\Desktop\ENEE630_F2022_TXsim\Input', p),k,1);
+    [~,e] = read_file(strcat('C:\Users\pasan\Desktop\ENEE630_F2022_TXsim\Input', p),k,1);
     de = [de e'];
 end
 
@@ -21,8 +13,17 @@ plot(1:length(dd),abs(dd),'r-',1:length(de),abs(de),'b--')
 
 figure;
 subplot(2,1,1);
-plot(abs(fft(dd,2^13))/5120,'r-');
+plot(abs(fft(dd,128))/5120,'r-');
 subplot(2,1,2);
-plot(abs(fft(de,2^13))/5120,'r-');
+plot(abs(fft(de,128))/5120,'r-');
 
 fprintf('test MSE: %f\n',MSE(dd,de,0,0,0))
+
+
+%%
+t = 0:1/1e4:10;
+a = cos(2*pi*2e3*t);
+b = exp(1i*2*pi*234.38*t);
+c = a.* b;
+figure;
+plot(abs(fft(c,128)))
