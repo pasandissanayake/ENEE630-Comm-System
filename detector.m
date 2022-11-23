e3ae836f76a824559c94dfce1ad4d74e127483f2
WiJE96 <lahiruwijerathna1996@gmail.com>
2022-11-23 08:16:49 -0500

    compensate any freq offset

--- detector.m
+++ detector.m
@@ -1,5 +1,6 @@
-function [freq_sign, freq_offset] = detector(burst_sig)
-% INPUT : burst_sig (complex)   - burst signal in the begining of the frame 
+function [freq_sign, freq_offset] = detector(burst_sig, fix_any)
+% INPUT :   burst_sig (complex)   - burst signal in the begining of the frame 
+%           fix_any (0/1)         - fix any frequency offset
 % OUTPUTS:
 %       freq_sign               - group freq signature
 %       freq_offset             - (group freq signature -
@@ -13,12 +14,28 @@ dft_out = abs(fft(burst_sig,128));
 % Detect burst freq
 [max_fft, max_bin] = max(dft_out);
 
+% Compensate for any freq offset
+if fix_any == 1
+    max_fft_m1 = dft_out(max_bin-1);
+    max_fft_p1 = dft_out(max_bin+1);
+    denom = max_fft_m1+max_fft_p1-2*max_fft;
+    if denom~=0
+        delta = (max_fft_m1 - max_fft_p1)/(max_fft_m1+max_fft_p1-2*max_fft);
+    end
+end
+
 % Calculate freq signature and freq offset
 max_bin = max_bin - 1;
 dist_arr = abs(group_bins - max_bin);
 [min_dist, min_dist_idx] = min(dist_arr);
 
 freq_sign = group_bins(min_dist_idx);
-freq_offset = max_bin - freq_sign;
+
+if fix_any == 1
+    true_freq = (max_bin + delta);
+    freq_offset = true_freq - freq_sign;
+else
+    freq_offset = max_bin - freq_sign;
+end
 
 end
