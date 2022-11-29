d1421e9030f40dfed257314ce4f9450c5450aadf
Pasan Dissanayake <pasandissanayake@gmail.com>
2022-11-29 12:05:19 -0500

    write_file completed

--- write_file.m
+++ write_file.m
@@ -3,7 +3,11 @@ function write_file(file_path,data)
     fprintf('write_file: writing to %s\n',file_path);
     for i=1:length(data)
         d = data(i);
-        fprintf(fileID,'%10.2f,  %10.2f\n\r',real(d),imag(d));
+        fprintf(fileID,'%10.2f,  %10.2f\n',real(d),imag(d));
+
+        if mod(i,1024)==0
+            fprintf(fileID,'\n\n');
+        end
     end
     fclose(fileID);
 end
