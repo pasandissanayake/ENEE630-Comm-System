398e6e59e29e4fae332d900fb0ccf2c8fac77b28
Pasan Dissanayake <pasandissanayake@gmail.com>
2022-11-30 09:17:29 -0500

    last commit before reease

--- onestagepp_comb.m
+++ onestagepp_comb.m
@@ -1,16 +1,7 @@
-global H0 H1 F0 F1
-H0 = [-1 0 3 0 -8 0 21 0 -45 0 91 0 -191 0 643 1024 643 0 -191 0 91 0 -45 0 21 0 -8 0 3 0 -1];
-H0 = [H0; [2048 zeros(1,30)]];
-H1 = H0; H1(1,16) = -1024;
-
-F0 = H0;
-F1 = H1; % F1(1,:) = -F1(1,:);
-
-disp("onestagepp MSE: " + MSE(10,1024,@onestagepp,1,1));
+disp("onestagepp MSE: " + MSE(@onestagepp,1,1,1000,1024));
 
 function [in, out] = onestagepp(x)
-    global H0 H1 F0 F1
+    [H0,H1,F0,F1] = filter_def();
     out = onestagepp_rx(onestagepp_tx(x,H0,H1),F0,F1);
-    delay = 15*2;
     in = x;
 end
